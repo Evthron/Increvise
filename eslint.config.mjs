2e2b805bb995d5481f07e1ed13e166691985b9c0
Evthron <116652843+Evthron@users.noreply.github.com>
2025-11-12 20:17:31 +0800

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js"
+import globals from "globals"
+import { defineConfig } from "eslint/config"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: {
+      js
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021
+      },
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    rules: {
+      "indent": ["error", 2],
+      "semi": ["error", "never"],
+      "prefer-const": "error"
+    }
+  }
+])
